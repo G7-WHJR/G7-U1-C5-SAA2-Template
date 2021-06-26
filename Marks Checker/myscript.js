9972376ce2ebf864dd4591c5d303b11204075234
G7-WHJR <raveen.anand@whitehatjr.com>
2021-06-26 09:51:31 +0530

    V1

---
+++ Marks Checker/myscript.js
@@ -0,0 +1,6 @@
+function checkGrade() {
+  //1. Take input from marks input field , store it in a variable
+  var marksInput = document.getElementById("marks").value;
+
+  //2. if marks are lesser than 30 display the message "Keep trying , you can do better!" else display "Congratulations , you scored well!"
+}
